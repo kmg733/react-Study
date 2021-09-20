cdfc531ae235022c748620c7fd3460e35167e53c
kmg733 <mang906@naver.com>
2021-09-20 15:50:55 +0900

    event state props and render func

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'welcome', 
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for information'},
@@ -18,6 +20,14 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       /* jshint ignore:start */
       <div className="App">
@@ -26,7 +36,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
       /* jshint ignore:end */
     );
